c0ca046332c0b4f3c9a8c7f2e953022584039c1a
hyunseo <leehs430@naver.com>
2022-01-07 19:21:17 +0900

    2022_01_07 19:21

--- concat.js
+++ concat.js
@@ -3,3 +3,5 @@ const array2 = ['d', 'e', 'f'];
 const array3 = array1.concat(array2);
 
 console.log(array3);
+
+// concat : 인자로 주어진 배열이나 값들을 기존 배열에 합쳐서 새 배열을 반환합니다.
--- unshift.js
+++ unshift.js
@@ -3,3 +3,6 @@ const array1 = [1, 2, 3];
 console.log(array1.unshift(4, 5));
 
 console.log(array1);
+
+// unshift : 새로운 요소를 배열의 맨 앞쪽에 추가하고, 새로운 길이를 반환합니다.
+
